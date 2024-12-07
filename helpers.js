64ec8852c57cb0448a7e9cf9f6c2662f39dedcea
Nadzeya Tsimashenkava <timoshenkova.nadya@gmail.com>
2024-12-07 23:56:54 +0300

    feat: add update method, update validation

--- helpers.js
+++ helpers.js
@@ -4,21 +4,21 @@ function checkId(id) {
 }
 
 function checkString (str) {
-    return str && typeof str === 'string';
+    return typeof str === 'string';
 }
 
 function checkRating (rating) {
-    return rating && typeof rating === 'string' && !isNaN(rating);
+    return typeof rating === 'string' && !isNaN(rating);
 }
 
 function checkYear (year) {
     const currentYear = new Date().getFullYear();
-    return year && Number.isInteger(year) && year >= 1895 && year <= currentYear
+    return Number.isInteger(year) && year >= 1895 && year <= currentYear
 
 }
 
 function checkNumber(num) {
-    return num && Number.isInteger(num) && num >= 0;
+    return Number.isInteger(num) && num >= 0;
 }
 
 
--- routes/router.js
+++ routes/router.js
@@ -46,43 +46,104 @@ filmsRouter.post("/create", (req, res, next) => {
       return next(new Error("Error of file riding"));
     }
     const { title, rating, year, budget, gross, poster, position } = req.body;
-    const films = JSON.parse(data);
-    const newId = Math.max(...films.map((film) => film.id)) + 1;
-    films.forEach((film) => {
-      if (film.position >= position) {
-        film.position += 1;
-      }
-    });
-    const newFilm = {
-      id: newId,
-      title,
-      rating,
-      year,
-      budget,
-      gross,
-      poster,
-      position,
-    };
-
     if (
+      !title ||
       !checkString(title) ||
+      !rating ||
       !checkRating(rating) ||
+      !year ||
       !checkYear(year) ||
+      !budget ||
       !checkNumber(budget) ||
+      !gross ||
       !checkNumber(gross) ||
+      !poster ||
       !checkString(poster) ||
+      !position ||
       !checkNumber(position)
     ) {
-        return next({ status: 400, message: "Input data is empty or has invalid value" });
-    }
+      return next({
+        status: 400,
+        message: "Input data is empty or has invalid value",
+      });
+    } else {
+      const films = JSON.parse(data);
+      const newId = Math.max(...films.map((film) => film.id)) + 1;
+      const newFilm = {
+        id: newId,
+        title,
+        rating,
+        year,
+        budget,
+        gross,
+        poster,
+        position,
+      };
+      films.forEach((film) => {
+        if (film.position >= position) {
+          film.position += 1;
+        }
+      });
       films.push(newFilm);
-    films.sort((a, b) => a.position - b.position);
-    console.log(films);
+      films.sort((a, b) => a.position - b.position);
+      res.status(201).json(newFilm);
+      fs.writeFile(filePath, JSON.stringify(films), (err) => {
+        if (err) throw err;
+      });
+    }
+  });
+});
 
-    res.status(201).json(newFilm);
-    fs.writeFile(filePath, JSON.stringify(films), (err) => {
-      if (err) throw err;
-    });
+filmsRouter.post("/update", (req, res, next) => {
+  fs.readFile(filePath, "utf8", (err, data) => {
+    if (err) {
+      return next(new Error("Error of file riding"));
+    }
+    const { id, title, rating, year, budget, gross, poster, position } =
+      req.body;
+    if (
+      (title && !checkString(title)) ||
+      (rating && !checkRating(rating)) ||
+      (year && !checkYear(year)) ||
+      (budget && !checkNumber(budget)) ||
+      (gross && !checkNumber(gross)) ||
+      (poster && !checkString(poster)) ||
+      (position && !checkNumber(position))
+    ) {
+      return next({
+        status: 400,
+        message: "Invalid input value",
+      });
+    } else {
+      const films = JSON.parse(data);
+      const currentElement = films.find(element => element.id === id);
+      const currentPosition = currentElement.position;
+      films.forEach((film) => {
+        if (film.position === position) {
+          film.position = currentPosition;
+        }
+      });
+     const updatedFilms = films.map((item) =>
+        item.id != id
+          ? item
+          : {
+              id: item.id,
+              title: title || item.title,
+              rating: rating || item.rating,
+              year: year || item.year,
+              budget: budget || item.budget,
+              gross: gross || item.gross,
+              poster: poster || item.poster,
+              position: position || item.position,
+            }
+      );
+      const updatedFilm = updatedFilms.find((element) => element.id == id);
+      updatedFilms.sort((a, b) => a.position - b.position);
+      res.status(201).json(updatedFilm);
+      fs.writeFile(filePath, JSON.stringify(updatedFilms), (err) => {
+        if (err) throw err;
+      });
+    }
   });
 });
 
